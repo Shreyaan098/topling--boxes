0708537811e6f5a8c76d7508f03a1ce1c872a30a
Shreyaan098 <70843525+Shreyaan098@users.noreply.github.com>
2020-10-10 16:52:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+const Engine =Matter.Engine;
+const World =Matter.World;
+const Bodies=Matter.Bodies
+
+var engine, world;
+var ground;
+var box1,box2,ground;
+
+function setup() {
+  var canvas = createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+
+  box1 = new Box(200,200,70,70);
+  box2= new  Box(180,10,70,50);
+  ground= new Ground(200,370,400,20)
+}
+
+function draw() {
+  background(0);  
+  Engine.update(engine);
+  box1.display();
+  box2.display();
+  ground.display();
+ }
